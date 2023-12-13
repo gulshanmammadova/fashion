b160b0e29da1a998c637f389f06a7ee29532e7d4
gulshanmammadova <gulshanfm@code.edu.az>
2023-12-13 10:17:41 -0800

    Header and Main Structor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route,Routes} from 'react-router-dom'
+import HomePage from './pages/Home/HomePage';
+import Error from './pages/Error/Error';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Blog from './pages/Blog/Blog';
+import Header from './components/Header/Navbar';
+import Footer from './components/Footer/Footer';
+import Shop from './pages/Shop/Shop'
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+    <Header/>
+<Routes>
+  <Route path='/' element={<HomePage/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/contact' element={<Contact/>}/>
+  <Route path='/blog' element={<Blog/>}/>
+  <Route path='/shop' element={<Shop/>}/>
+  <Route path='*' element={<Error/>}/>
+
+
+  
+</Routes>
+<Footer/>
     </div>
   );
 }
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+import {  HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
+import CodeIcon from '../../images/logo/logo.png'
+import { CiUser } from "react-icons/ci";
+import { CiShoppingBasket } from "react-icons/ci";
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            <img src={CodeIcon} alt="Logo" />
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/blog"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/shop"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Shop
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="icon1 icon">
+            <CiUser/>
+            
+            <ul className="list-account"> 
+              <li>Login</li>
+              <li>Register</li>
+              <li>Log Out</li>
+            </ul>
+            </li>
+            <li className="icon2 icon">
+         <CiShoppingBasket/>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
+
+            {click ? (
+              <span className="icon">
+                <HamburgetMenuOpen />{" "}
+              </span>
+            ) : (
+              <span className="icon">
+                <HamburgetMenuClose />
+              </span>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
