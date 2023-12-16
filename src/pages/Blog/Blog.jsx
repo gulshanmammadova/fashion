ee2a444256ff0dd473662354c9b88a185ebb5451
gulshanmammadova <gulshanfm@code.edu.az>
2023-12-16 07:42:30 -0800

    blog ikili filter

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,45 +1,96 @@
-import React from 'react'
-// Bootstrap CSS
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import datas from '../../data/blog';
 import Card from 'react-bootstrap/Card';
-import {Link} from 'react-router-dom'
-import './Blog.css'
+import { Link } from 'react-router-dom';
+import './Blog.css';
+
 const Blog = () => {
-  datas.forEach(element => {
-      console.log(element.tags)
-      
-    });
+  const [tags, setTags] = useState([]);
+  const [searchByTags, setSearchByTags] = useState([]);
+  const [searchByNames, setSearchByNames] = useState([]);
+  const [inp, setInp] = useState('');
+
+  useEffect(() => {
+    const allTags = datas.reduce((accumulator, currentBlog) => {
+      currentBlog.tags.forEach(tag => {
+        if (!accumulator.includes(tag)) {
+          accumulator.push(tag);
+        }
+      });
+      return accumulator;
+    }, []);
+
+    setTags(allTags);
+  }, []);
+
+  useEffect(() => {
+    tagHandle();
+  }, [inp]);
+
+  const inpHandle = (e) => {
+    setInp(e.target.value);
+  };
+
+  const tagHandle = (tag) => {
+   
+    let filterByName = datas.filter(
+      n => n.title.trim().toLowerCase().includes(inp.trim().toLowerCase())
+    );
+
+    if (searchByNames.length > 0 && searchByTags.length > 0 ) {
+      let filteredResults = searchByNames.filter(blog =>
+        blog.tags.includes(tag));
+        console.log(filterByName.filter(blog =>blog.tags.includes(tag)));
+       setSearchByNames(filteredResults);
+    } 
+    else if (searchByNames.length > 0) {
+      setSearchByNames(filterByName);
+    } 
+    else if (searchByTags.length > 0) {
+      setSearchByNames(searchByTags);
+    }
+     else {
+      setSearchByNames(datas); 
+        }
+        // console.log(filterByName);
+        
+  };
+
+ 
   return (
-
     <div className='container'>
-      <div className='blog-cards-all'>
-      <div className='d-flex   col-lg-8' style={{justifyContent:'space-between',flexWrap:'wrap'}}>
-      {
-        datas.map((blog,index)=>{
-          return(<Card className=' mx-2 my-2 b-0 card-1' style={{width:270}} key={index}>
-          <Card.Img  className='b-0' variant="top" src={blog.mainImg} />
-          <Card.Body>
-            <Card.Title className='blog-card-title'>{blog.date} | {blog.author}</Card.Title>
-            <Card.Text>
-            {blog.title}
-            </Card.Text>
-            <Link className=' detail-blog' to={`/singleblog/${blog.id}`}> Read More ...</Link>
-
-             </Card.Body>
-        </Card>)
-        })
-}
-      </div>
-      <div className='filter col-lg-2'>
-
-      </div>
-
+      <div className='blog-cards-all d-flex'>
+        <div className='d-flex col-lg-8' style={{ justifyContent: 'space-between', flexWrap: 'wrap' }}>
+          {searchByNames.map((blog, index) => (
+            <Card className='mx-2 my-2 b-0 card-1' style={{ width: 270 }} key={index}>
+              <Card.Img className='b-0' variant="top" src={blog.mainImg} />
+              <Card.Body>
+                <Card.Title className='blog-card-title'>{blog.date} | {blog.author}</Card.Title>
+                <Card.Text>{blog.title}</Card.Text>
+                <Link className='detail-blog' to={`/singleblog/${blog.id}`}> Read More ...</Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+        <div className='filter col-lg-2'>
+          <input type="text" onChange={inpHandle} />
+          <div>
+            <h3>Tags</h3>
+            <ul>
+              {tags.map((x, i) => (
+                <li className='li-tag' key={i} onClick={() => tagHandle(x)}
+                  style={{ color: 'rgb(92, 163, 163) !important', listStyleType: 'none', marginTop: '-20' }}>
+                  {x}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
