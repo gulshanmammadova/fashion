7c61aec7757532964dff14654d86155fa42c4a06
gulshanmammadova <gulshanfm@code.edu.az>
2023-12-24 12:26:28 -0800

    Last Commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import NewsLetter from '../../components/NewsLetter/NewsLetter';
 import UpIcon from '../../components/Up/UpIcon.jsx';
-
 const Contact = () => {
   return (
     <div className=''>
